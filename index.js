b9d8dc0ab3777ab789299fde8ebc719d63d7f4a5
David Moina <davemolina777@gmail.com>
2021-04-28 13:14:42 -0500

    commit tree f78f23f0026177df1763d85d3f9752f981f3b7d4

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.querySelector("main")
+element.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.className = "victory"
+newHeader.id = "victory"
+newHeader.innerHTML = "David is the champion"
